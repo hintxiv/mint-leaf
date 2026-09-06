d73e2e09397f117f79b430c5aed802bb7ad09826
hintxiv <65056303+hintxiv@users.noreply.github.com>
2026-09-06 01:50:28 -0400

    Add remaining job catalogs and remove catalog documentation tooling (#23)

--- src/data/actionCatalog/index.ts
+++ src/data/actionCatalog/index.ts
@@ -1,9 +1,53 @@
+import ast from './jobs/AST.json'
+import blm from './jobs/BLM.json'
+import blu from './jobs/BLU.json'
+import brd from './jobs/BRD.json'
+import dnc from './jobs/DNC.json'
+import drg from './jobs/DRG.json'
+import drk from './jobs/DRK.json'
+import gnb from './jobs/GNB.json'
 import mch from './jobs/MCH.json'
+import mnk from './jobs/MNK.json'
+import nin from './jobs/NIN.json'
+import pct from './jobs/PCT.json'
+import pld from './jobs/PLD.json'
+import rdm from './jobs/RDM.json'
+import rpr from './jobs/RPR.json'
+import sam from './jobs/SAM.json'
+import sch from './jobs/SCH.json'
+import sge from './jobs/SGE.json'
+import smn from './jobs/SMN.json'
+import vpr from './jobs/VPR.json'
+import war from './jobs/WAR.json'
+import whm from './jobs/WHM.json'
 import roles from './roles.json'
 import statusData from './statuses.json'
 import { CatalogAction, CatalogStatus, JobCatalog } from './types'
 
-export const catalogs: Record<string, JobCatalog> = { MCH: mch as JobCatalog }
+export const catalogs: Record<string, JobCatalog> = {
+    AST: ast as JobCatalog,
+    BLM: blm as JobCatalog,
+    BLU: blu as JobCatalog,
+    BRD: brd as JobCatalog,
+    DNC: dnc as JobCatalog,
+    DRG: drg as JobCatalog,
+    DRK: drk as JobCatalog,
+    GNB: gnb as JobCatalog,
+    MCH: mch as JobCatalog,
+    MNK: mnk as JobCatalog,
+    NIN: nin as JobCatalog,
+    PCT: pct as JobCatalog,
+    PLD: pld as JobCatalog,
+    RDM: rdm as JobCatalog,
+    RPR: rpr as JobCatalog,
+    SAM: sam as JobCatalog,
+    SCH: sch as JobCatalog,
+    SGE: sge as JobCatalog,
+    SMN: smn as JobCatalog,
+    VPR: vpr as JobCatalog,
+    WAR: war as JobCatalog,
+    WHM: whm as JobCatalog,
+}
 export const catalogStatuses = statusData.statuses as CatalogStatus[]
 
 export const findCatalogAction = (job: string, id: string): CatalogAction | undefined => {
--- src/data/actionCatalog/types.ts
+++ src/data/actionCatalog/types.ts
@@ -1,10 +1,3 @@
-export interface Source {
-    id: string
-    url: string
-    revision: string
-    verifiedAt: string
-}
-
 export interface CatalogStatus {
     id: number
     icon: string
@@ -22,22 +15,14 @@ export interface CatalogAction {
     kind?: 'gcd' | 'ogcd'
     baseCastTimeMs?: number
     gcdRecastOverrideMs?: number
-    gcdRecastOverrideReason?: string
     abilityCooldownMs?: number
     speedCategory?: 'skill' | 'spell' | 'fixed'
     statuses: AppliedStatus[]
-    unsupported?: string
 }
 
 export interface JobCatalog {
-    schemaVersion: number
     revision: string
     job: string
-    level: number
-    patch: string
-    verifiedAt: string
-    sources: Source[]
     actions: CatalogAction[]
     roleActionIds: number[]
-    inventory: { id: number; disposition: 'configured' | 'unsupported'; reason?: string }[]
 }
